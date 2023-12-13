7dd9a8cca6861f2259c0dbc6629948e829a0c655
gabegalindo <116311646+gabegalindo@users.noreply.github.com>
2023-12-12 20:36:39 -0800

    Add files via upload

---
+++ assignments.js
@@ -0,0 +1,74 @@
+
+// const country = "Argentina";
+// const continent = "South America";
+// let population = 46;
+// console.log(typeof country);
+// // console.log(continent);
+// console.log(typeof population);
+
+
+// const isIsland = false;
+// let language = 'spanish';
+
+// console.log(typeof isIsland);
+// console.log(typeof language);
+
+// console.log(population / 2);
+// population++;
+// console.log(population);
+
+// const finland = 6;
+// console.log(population > finland);
+// let avgPop = 33;
+// console.log(population < avgPop);
+
+// const description = country + " is in " + continent + ", and its " + population + " million people speak " + language;
+// console.log(description);
+
+// const descriptionNew = `${country} is in ${continent}, and its ${population} million people speak ${language}`;
+// console.log(descriptionNew);
+
+// if (population > avgPop) {
+//     console.log(`${country}'s population is above average`);
+// } else {
+//     console.log(`${country}'s population is ${avgPop - population} million below average`);
+// }
+
+// // const numNeighbours = Number(prompt("How many neighbour countries does your country have?"));
+// // if (numNeighbours === 1) {
+// //     console.log("Only 1 border!");
+// // } else if (numNeighbours > 1) {
+// //     console.log("More than one border!");
+// // }
+// // else if (numNeighbours === 0) {
+// //     console.log("No borders");
+// // }
+
+// if (language === 'spanish' && population < 50 && !isIsland) {
+//     console.log('You should live in Argentina :)');
+// } else {
+//     console.log('Argentina does not meet your criteria :(');
+// }
+
+// switch (language) {
+//     case 'mandarin':
+//         console.log('MOST number of native speakers!');
+//         break;
+//     case 'spanish':
+//         console.log('2nd place in number of native speakers');
+//         break;
+//     case 'english':
+//         console.log('3rd place');
+//         break;
+//     case 'hindi':
+//         console.log('Number 4');
+//         break;
+//     case 'arabic':
+//         console.log('5th most spoken language');
+//         break;
+//     default:
+//         console.log('Great language too :)');
+//         break;
+// }
+
+// population >= 33 ? console.log("Argentina's population is above average") : console.log("Argentina's population is below average")
